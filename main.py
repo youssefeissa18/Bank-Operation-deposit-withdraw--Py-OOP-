7efd4693ce48f53820f05bbdb37c27ef1e6c9ed4
Youssef Abd-El-Ghfar Abd-El-Twab <91377316+youssefeissa18@users.noreply.github.com>
2023-09-23 14:42:29 +0300

    Create Class For Bank Operation and Make Constructor to create new account and Function to Deposit and withdraw and mini statement

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+#Bank Operation
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Bank:
+    bankName = "Bank Masr"
+    Address = "Badrashin"
+
+    # Create Account                                                   # Password
+    def __init__(self, userName, userAddress, phoneNumber, nationalID, cardNumber):
+        self.userName = userName
+        self.userAddress = userAddress
+        self.phoneNumber = phoneNumber
+        self.nationalID = nationalID
+        self.cardNumber = cardNumber
+        self.Balance = 0.0
+        print(f"Hello {userName} Your Account Creation Successfully")
+
+    def Deposit(self,amount):
+        self.Balance += amount
+        print(f"{amount} Deposit Successfully")
+
+    def Withdraw(self,amount):
+        if amount < self.Balance:
+            self.Balance -= amount
+            print(f"{amount} Withdraw Successfully")
+        else:
+            print("Your Balance don't Have Enough money to Withdraw")
+
+    def miniStatement(self):
+        print(f"Your Account Have {self.Balance}")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(f"Welcome {Bank.bankName} Branch {Bank.Address}")
+name = input("Enter Your User Name : ")
+address = input("Enter Your User Address : ")
+PhoneNumber = input("Enter Your User Phone Number : ")
+NationalID = input("Enter Your User National ID : ")
+CardNumber = int(input("Enter Your User Card Number  : "))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+Client = Bank(name, address, PhoneNumber, NationalID, CardNumber)
+
+while True:
+    print('\nPlease Select any Option : ')
+    print('1.Deposit: \n2.Withdraw: \n3.Minis Statement: \n4.Stop: ')
+    option = int(input(' '))
+    if option == 1:
+        amount = float(input('Enter Deposited amount : '))
+        Client.Deposit(amount)
+
+    elif option == 2:
+        amount = float(input('Enter Withdraw amount : '))
+        Client.Withdraw(amount)
+
+    elif option == 3:
+        Client.miniStatement()
+
+    elif option == 4:
+        print('Thanks for using Bank Masr.... ')
+        break
+    else:
+        print('Invalid Option plz select Another Option')
